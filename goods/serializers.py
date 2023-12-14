4cbabbff32e1fa35b184cec61ffe539118387a00
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2023-12-14 18:55:42 +0700

    changes

--- goods/serializers.py
+++ goods/serializers.py
@@ -7,6 +7,11 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category
         fields = '__all__'
 
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['category_img'] = representation['category_img'].replace('https', 'http')
+        return representation
+
 
 class ProductSerializer(serializers.ModelSerializer):
     category = CategorySerializer()
@@ -14,6 +19,12 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['product_img'] = representation['product_img'].replace('https', 'http')
+        return representation
+
 
 class AddProductSerializer(serializers.ModelSerializer):
     class Meta:
