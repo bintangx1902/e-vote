87dfb717b2a73f16724302f22aca18843d93e4c3
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2023-12-14 14:25:10 +0700

    new urls

--- goods/urls.py
+++ goods/urls.py
@@ -4,4 +4,6 @@ from .views import *
 urlpatterns = [
     path('category',CategoryListEndPoint.as_view(),name='category-list'),
     path('product', ProductListEndPoint.as_view(), name='product-list'),
+    path('add-product', add_new_product, name='add-product'),
+    path('add-category', add_new_category, name='add-category')
 ]
--- goods/views.py
+++ goods/views.py
@@ -1,4 +1,5 @@
 from rest_framework import status
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.generics import *
 from rest_framework.response import Response
 from .serializers import *
@@ -19,13 +20,39 @@ class CategoryListEndPoint(ListAPIView):
 
 
 class ProductListEndPoint(ListAPIView):
-    queryset = Product.objects.all()
     permission_classes = (IsAuthenticated,)
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
     def list(self, *args, **kwargs):
-        q = self.get_queryset()
-        ser = self.serializer_class(q, many=True, context={'request': self.request})
-        if q.exists():
+        query = self.get_queryset()
+        ser = self.serializer_class(query, many=True, context={'request': self.request})
+        if query.exists():
             return Response(ser.data, status=status.HTTP_200_OK)
         return Response({"msg": "not found"}, status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def add_new_product(request):
+    if request.method == 'POST':
+        data = request.data
+        serializer = AddProductSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"msg": "Berhasil Tambah Produk"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return Response()
+
+
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def add_new_category(request):
+    if request.method == 'POST':
+        data = request.data
+        serializer = CategorySerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"msg": "Berhasil Tambah Kategori"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return Response()
